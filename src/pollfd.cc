4c078591f576fea62bfb0408899c8d5b732191a8
Arvin Hsu <arvin.x@icloud.com>
2019-05-11 19:09:14 +0800

    Add a basic Pollfd class with no interaction with event loop

---
+++ src/pollfd.cc
@@ -0,0 +1,53 @@
+#include "pollfd.hh"
+#include "eventloop.hh"
+#include "util/log.hh"
+
+namespace axs {
+
+PollFd::~PollFd() {
+    assert(!event_handling_);
+    ::close(fd_);
+}
+
+void PollFd::HandleEvent() {
+    event_handling_ = true;
+    LOG_INFO << "Handle event: " << EventsToStr(revents_) << "on fd: " << fd_;
+    if (revents_ & EPOLLERR) {
+        if (err_cb_) err_cb_();
+    }
+    // Leave HUP events to reading callback because sometimes hanging up does
+    // not trigger EPOLLHUP or EPOLLRDHUP, it only triggers normal EPOLLIN.
+    if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP | EPOLLHUP)) {
+        if (read_cb_) read_cb_();
+    }
+    if (revents_ & EPOLLOUT) {
+        if (write_cb_) write_cb_();
+    }
+    event_handling_ = false;
+}
+
+void PollFd::RemoveFromLoop() {
+}
+
+void PollFd::NotifyLoop() {
+}
+
+std::string PollFd::EventsToStr(int events) {
+    std::string events_str_{};
+    if (events & EPOLLIN) {
+        events_str_ += "IN ";
+    } else if (events & EPOLLOUT) {
+        events_str_ += "OUT ";
+    } else if (events & EPOLLRDHUP) {
+        events_str_ += "RDHUP ";
+    } else if (events & EPOLLPRI) {
+        events_str_ += "PRI ";
+    } else if (events & EPOLLERR) {
+        events_str_ += "ERR ";
+    } else if (events & EPOLLHUP) {
+        events_str_ += "HUP ";
+    }
+    return events_str_;
+}
+
+}
---
+++ src/pollfd.hh
@@ -0,0 +1,65 @@
+#ifndef _AXS_POLLFD_HH_
+#define _AXS_POLLFD_HH_
+
+#include <string>
+#include <functional>
+#include <boost/core/noncopyable.hpp>
+#include <sys/epoll.h>
+
+namespace axs {
+
+// Forward declaration.
+class EventLoop;
+
+// Responsible for the management of the life cycle of the contained fd.
+class PollFd : private boost::noncopyable {
+public:
+    using EventCallback = std::function<void()>;
+
+    PollFd(EventLoop& loop, int fd) : loop_{loop}, fd_{fd} {}
+    ~PollFd();
+
+    // Trivial getters and setters.
+    int Fd() const { return fd_; }
+    int Events() const { return events_; }
+    void SetRevents(int revents) { revents_ = revents; }
+
+    // Event switchers.
+    void EnableReading() { events_ |= kETRead; NotifyLoop(); }
+    void DisableReading() { events_ &= ~kETRead; NotifyLoop(); }
+    void EnableWriting() { events_ |= kETWrite; NotifyLoop(); }
+    void DisableWriting() { events_ &= ~kETWrite; NotifyLoop(); }
+    bool IsReading() const { return events_ & kETRead; }
+    bool IsWriting() const { return events_ & kETWrite; }
+    bool CareNoEvent() const { return events_ == 0; }
+
+    // Callback setters.
+    void SetReadCallback(EventCallback cb) { read_cb_ = cb; }
+    void SetWriteCallback(EventCallback cb) { write_cb_ = cb; }
+    void SetErrorCallback(EventCallback cb) { err_cb_ = cb; }
+
+    // Non-trivial member functions.
+    void HandleEvent();
+    void RemoveFromLoop();
+
+private:
+    enum EventType {
+        kETRead = EPOLLIN | EPOLLPRI,
+        kETWrite = EPOLLOUT
+    };
+
+    void NotifyLoop();
+    static std::string EventsToStr(int events);
+
+    EventLoop& loop_;
+    int fd_;
+    int events_{0};
+    int revents_{0};
+    EventCallback read_cb_{};
+    EventCallback write_cb_{};
+    EventCallback err_cb_{};
+    bool event_handling_{false};
+};
+
+}
+#endif
