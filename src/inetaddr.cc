096e987e5e41163ab6f3d65ef696b1241a1c43fc
Arvin Hsu <arvin.x@icloud.com>
2019-05-16 16:36:48 +0800

    Improve InetAddr class

--- src/inetaddr.cc
+++ src/inetaddr.cc
@@ -6,11 +6,24 @@
 
 namespace axs {
 
+static_assert(sizeof(InetAddr) == sizeof(struct sockaddr_in),
+              "InetAddr and struct sockaddr_in are different sizes");
+
 InetAddr::InetAddr(const std::string& ip, std::uint16_t port) {
     addr_.sin_family = AF_INET;
     addr_.sin_port = htobe16(port);
     if (::inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr) < 0)
         LOG_ERROR << "Illegal ip address: " << ip;
+}
+
+std::uint16_t InetAddr::Port() const {
+    return be16toh(addr_.sin_port);
+}
+
+std::string InetAddr::Ip() const {
+    char ip[64];
+    ::inet_ntop(AF_INET, &addr_.sin_addr, ip, sizeof(ip));
+    return ip;
 }
 
 }
--- src/inetaddr.hh
+++ src/inetaddr.hh
@@ -1,17 +1,28 @@
 #ifndef _AXS_INETADDR_HH_
 #define _AXS_INETADDR_HH_
 
+#include <string>
 #include <cstdlib>
+#include <sys/socket.h>
 #include <netinet/in.h>
 
 namespace axs {
 
 class InetAddr {
 public:
+    // Construct an empty struct sockaddr_in used to be filled by accept() etc.
+    InetAddr() {}
     InetAddr(const std::string& ip, std::uint16_t port);
     InetAddr(const struct sockaddr_in& addr) : addr_{addr} {}
     const struct sockaddr* SockAddr() const {
-        return static_cast<struct sockaddr*>(&addr); }
+        return reinterpret_cast<const struct sockaddr*>(&addr_); }
+    struct sockaddr* SockAddr() {
+        return reinterpret_cast<struct sockaddr*>(&addr_); }
+    socklen_t SockAddrLen() const {
+        return static_cast<socklen_t>(sizeof(addr_)); }
+    // Get printable address.
+    std::uint16_t Port() const;
+    std::string Ip() const;
 private:
     struct sockaddr_in addr_{};
 };
